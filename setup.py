7bded0ad4059d1c5465be05d64f5bee67529751a
Sören Kirchner <sk@tologo.de>
2019-10-26 02:32:39 +0200

    version

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import setuptools
 setuptools.setup(
     name="pyimgbatch",
     packages=['pyimgbatch'],
+    version_config={
+        "version_format": "{tag}.dev{sha}",
+        "starting_version": "0.1.0"
+    },
+    setup_requires=['better-setuptools-git-version'],
     entry_points={
         'console_scripts': [
             'pyimgbatch = pyimgbatch.__main__:main'
