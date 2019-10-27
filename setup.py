2cfe2d978cb86a5904adc761acad64f89911a464
Sören Kirchner <sk@tologo.de>
2019-10-27 22:05:32 +0100

    config for distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,35 @@
 import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 setuptools.setup(
     name="pyimgbatch",
-    packages=['pyimgbatch'],
+    author="Sören Kirchner",
+    author_email="sk@tologo.de",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/soeren-kirchner/pyimgbatch",
+    #packages=['pyimgbatch'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Other Audience",
+        "Programming Language :: Python :: Implementation",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ],
+    python_requires='>=3.6',
     version_config={
         "version_format": "{tag}.dev{sha}",
         "starting_version": "0.1.0"
