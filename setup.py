7a6f48bf562817c123c2905efa72ef846ac505de
Sören Kirchner <sk@tologo.de>
2019-10-31 17:35:21 +0100

    whitespace for linter

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/soeren-kirchner/pyimgbatch",
-    #packages=['pyimgbatch'],
+    # packages=['pyimgbatch'],
     install_requires=requirements,
     packages=setuptools.find_packages(),
     classifiers=[
